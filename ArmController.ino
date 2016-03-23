c27663a598a98ab8ad303d9812739107cd8b2318
Ben Brockway <ben@Benjamins-MacBook-Air.local>
2016-03-23 19:31:57 -0400

    set up code so that buttons will read for long enough to be registered by the drive station and olny update once per state change.

--- ArmController.ino
+++ ArmController.ino
@@ -18,11 +18,17 @@ int button2 = 7;
 int button3 = 8;
 int button4 = 9;
 int button5 = 10;
-int buttonState1 = 0;
-int buttonState2 = 0;
-int buttonState3 = 0;
-int buttonState4 = 0;
-int buttonState5 = 0;
+int currentButtonState1 = 0;
+int currentButtonState2 = 0;
+int currentButtonState3 = 0;
+int currentButtonState4 = 0;
+int currentButtonState5 = 0;
+int previousButtonState1 = 0;
+int previousButtonState2 = 0;
+int previousButtonState3 = 0;
+int previousButtonState4 = 0;
+int previousButtonState5 = 0;
+int buttonDelay = 20;
 
 
 void setup() {
@@ -32,6 +38,13 @@ void setup() {
   pinMode(button3, INPUT);
   pinMode(button4, INPUT);
   pinMode(button5, INPUT);
+
+  //in this section we read the initial state off the buttons
+  previousButtonState1 = digitalRead(button1);
+  previousButtonState2 = digitalRead(button2);
+  previousButtonState3 = digitalRead(button3);
+  previousButtonState4 = digitalRead(button4);
+  previousButtonState5 = digitalRead(button5);
 
   // Initialize Joystick Library
   Joystick.begin();
@@ -45,45 +58,71 @@ int lastButtonState[5] = {0, 0, 0, 0, 0};
 
 void loop() {
 
-buttonState1 = digitalRead(button1);
-if (buttonState1 == HIGH) {
-  Joystick.setButton(0, 1);
-}
-else if (buttonState1 == LOW) {
-  Joystick.setButton(0, 0);
+currentButtonState1 = digitalRead(button1);
+if (currentButtonState1 != previousButtonState1) {
+  previousButtonState1 = currentButtonState1;
+  if (currentButtonState1 == LOW) {
+    delay(buttonDelay);
+  }
+  Joystick.setButton(0, currentButtonState1);
+ //else if (currentButtonState1 == HIGH) {
+  //Joystick.setButton(0, currentButtonState1);
+  //}
+
 }
 
-buttonState2 = digitalRead(button2);
-if (buttonState2 == HIGH) {
-  Joystick.setButton(1, 1);
+currentButtonState2 = digitalRead(button2);
+if (currentButtonState2 != previousButtonState2) {
+  previousButtonState2 = currentButtonState2;
+  if (currentButtonState2 == LOW) {
+    delay(buttonDelay);
+  }
+  Joystick.setButton(1, currentButtonState2); 
 }
+/*}
 else if (buttonState2 == LOW) {
+  delay(buttonDelay);
   Joystick.setButton(1, 0);
-}
+}*/
 
-buttonState3 = digitalRead(button3);
-if (buttonState3 == HIGH) {
-  Joystick.setButton(2, 1);
+currentButtonState3 = digitalRead(button3);
+if (currentButtonState3 != previousButtonState3) {
+  previousButtonState3 = currentButtonState3;
+  if (currentButtonState3 == LOW) {
+    delay(buttonDelay);
+  }
+  Joystick.setButton(2, currentButtonState3); 
 }
-else if (buttonState3 == LOW) {
+/*else if (buttonState3 == LOW) {
+  delay(buttonDelay);
   Joystick.setButton(2, 0);
-}
+}*/
 
-buttonState4 = digitalRead(button4);
-if (buttonState4 == HIGH) {
-  Joystick.setButton(3, 1);
+currentButtonState4 = digitalRead(button4);
+if (currentButtonState4 != previousButtonState4) {
+  previousButtonState4 = currentButtonState4;
+  if (currentButtonState4 == LOW) {
+    delay(buttonDelay);
+  }
+  Joystick.setButton(3, currentButtonState4); 
 }
-else if (buttonState4 == LOW) {
+/*else if (buttonState4 == LOW) {
+  delay(buttonDelay);
   Joystick.setButton(3, 0);
-}
+}*/
 
-buttonState5 = digitalRead(button5);
-if (buttonState5 == HIGH) {
-  Joystick.setButton(4, 1);
+currentButtonState5 = digitalRead(button5);
+if (currentButtonState5 != previousButtonState5) {
+  previousButtonState5 = currentButtonState5;
+  if (currentButtonState5 == LOW) {
+    delay(buttonDelay);
+  }
+  Joystick.setButton(4, currentButtonState5); 
 }
-else if (buttonState5 == LOW) {
+/*else if (buttonState5 == LOW) {
+  delay(buttonDelay);
   Joystick.setButton(4, 0);
-}
+}*/
 
   // Read pin values
   /*for (int index = 0; index < 12; index++)
